84584b03847f98a2d13ba0a6910390c904b8cc21
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2024-02-13 07:23:12 -0800

    Add test

---
+++ scripts/tests/test-mtree.py
@@ -0,0 +1,34 @@
+import unittest
+import torch
+import cProfile
+
+from scripts.modules.mtree import *
+
+class MTreeUnitTest(unittest.TestCase):
+    
+    def test_all(self):
+        with cProfile.Profile() as pr:
+            data = torch.randn((2000, 128))
+            data.div_(data.norm(dim=1,keepdim=True))
+        
+            mtree = MTree(lambda x, y: (x - y).norm().item())
+        
+            for i in range(data.shape[0]):
+                dist, val = mtree.get_nearest(data[i])
+                mtree.add_point(data[i])
+               
+        pr.print_stats('time')
+        
+        test_data = torch.randn((20, 128))
+        test_data.div_(test_data.norm(dim=1,keepdim=True))
+        
+        for i in range(test_data.shape[0]):
+            truth_dist, truth_idx = (data - test_data[i]).norm(dim=1).min(dim=0)
+            truth_val = data[truth_idx]
+
+            dist, val = mtree.get_nearest(test_data[i])
+            self.assertAlmostEqual(dist, truth_dist.item())
+            self.assertTrue(torch.allclose(val, truth_val))
+            
+if __name__ == '__main__':
+    unittest.main()
