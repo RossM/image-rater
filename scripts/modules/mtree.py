525a81c4d69b2f49613bcbf54a131463c0564a2c
Ross Morgan-Linial <ross.morgan.linial@gmail.com>
2024-02-13 19:58:53 -0800

    Real tree! Optimize

--- scripts/modules/mtree.py
+++ scripts/modules/mtree.py
@@ -3,55 +3,78 @@ from typing import Any, Callable, List, Tuple
 import torch
 from torch import Tensor
 
+
 class MTree:
     class Node:
         _points: Tensor
-        _children: List['MTree.Node']
-        _radius: Tensor
+        _children: List["MTree.Node"]
+        _radii: Tensor
         _count: int
 
-        def __init__(self, points: Tensor):
+        def __init__(self, points: Tensor) -> None:
             self._points = points
-            self._children = []
-            self._radius = torch.tensor(0, dtype=points.dtype)
+            self._children = None
+            self._radii = None
             self._count = points.shape[0]
-        
-        def add_point(self, tree: 'MTree', point: Tensor):
-            self._points = torch.cat((self._points, point[None, :]), dim=0)
-            self._radius.clamp_(min=tree._dist_func(self._points[0], point))
-            self._count += 1
 
-        def get_nearest(self, tree: 'MTree', point: Tensor, best_dist: Tensor, best_point: Tensor) -> Tuple[Tensor, Tensor]:
+        def add_point(self, tree: "MTree", point: Tensor) -> None:
+            self._count += 1
+            if self._points.shape[0] < tree._max_node_size:
+                self._points = torch.cat((self._points, point[None, :]), dim=0)
+            else:
+                if self._children == None:
+                    self._children = [None] * self._points.shape[0]
+                    self._radii = self._points.new_zeros((self._points.shape[0]))
+
+                _, index = tree._dist_func(point[None, :], self._points).min(dim=0)
+                if self._children[index] == None:
+                    self._children[index] = MTree.Node(point[None, :])
+                else:
+                    self._children[index].add_point(tree, point)
+                    self._radii[index].clamp_(min=tree._dist_func(self._points[0], point))
+
+        def get_nearest(
+            self, tree: "MTree", point: Tensor, best_dist: Tensor, best_point: Tensor
+        ) -> Tuple[Tensor, Tensor]:
             distances = tree._dist_func(self._points, point[None, :])
-            my_dist, my_idx = distances.min(dim=0)
-            if my_dist < best_dist:
-                best_dist, best_point = my_dist, self._points[my_idx]
-          
-            for child in self._children:
-                child_dist = tree._dist_func(child._points[0], point)
-                if child_dist < child._radius + best_dist:
-                    best_dist, best_point = child.get_nearest(tree, point, best_dist, best_point)
-                    
+            _, indexes = distances.sort()
+
+            if distances[indexes[0]] < best_dist:
+                best_dist, best_point = distances[indexes[0]], self._points[indexes[0]]
+
+            if self._children != None:
+                for index in indexes:
+                    child = self._children[index]
+                    if child != None and distances[index] < self._radii[index] + best_dist:
+                        best_dist, best_point = child.get_nearest(
+                            tree, point, best_dist, best_point
+                        )
+
             return best_dist, best_point
 
-
-    _root: 'MTree.Node'
+    _root: "MTree.Node"
     _dist_func: Callable[[Tensor, Tensor], Tensor]
+    _max_node_size: int
 
-    def __init__(self, dist_func: Callable[[Tensor, Tensor], Tensor] = None):
+    def __init__(
+        self,
+        dist_func: Callable[[Tensor, Tensor], Tensor] = None,
+        max_node_size: int = 64,
+    ):
         def torch_distance(x: Tensor, y: Tensor) -> Tensor:
             return (x - y).norm(dim=-1)
 
         self._root = None
         self._dist_func = dist_func or torch_distance
-        
+        self._max_node_size = max_node_size
+
     @torch.no_grad()
     def add_point(self, point: Tensor | list[float]):
         if isinstance(point, list):
             point = torch.tensor(point)
 
         if self._root == None:
-            self._root = MTree.Node(point[None,:])
+            self._root = MTree.Node(point[None, :])
         else:
             self._root.add_point(self, point)
 
--- scripts/tests/test-mtree.py
+++ scripts/tests/test-mtree.py
@@ -4,25 +4,38 @@ import cProfile
 
 from scripts.modules.mtree import *
 
+
 class MTreeUnitTest(unittest.TestCase):
-    
+
+    def test_profile_mtree(self):
+
+        data = torch.randn((2000, 128))
+        data.div_(data.norm(dim=1, keepdim=True))
+
+        for max_node_size in [32, 64, 128]:
+            with cProfile.Profile() as pr:
+                mtree = MTree(max_node_size=max_node_size)
+
+                for i in range(data.shape[0]):
+                    dist, val = mtree.get_nearest(data[i])
+                    mtree.add_point(data[i])
+
+            print(f"max_node_size = {max_node_size}")
+            pr.print_stats("time")
+
     def test_mtree(self):
 
-        with cProfile.Profile() as pr:
-            data = torch.randn((5000, 128))
-            data.div_(data.norm(dim=1,keepdim=True))
-        
-            mtree = MTree()
-        
-            for i in range(data.shape[0]):
-                dist, val = mtree.get_nearest(data[i])
-                mtree.add_point(data[i])
-               
-        pr.print_stats('time')
-        
+        data = torch.randn((1000, 128))
+        data.div_(data.norm(dim=1, keepdim=True))
+
+        mtree = MTree()
+
+        for i in range(data.shape[0]):
+            mtree.add_point(data[i])
+
         test_data = torch.randn((20, 128))
-        test_data.div_(test_data.norm(dim=1,keepdim=True))
-        
+        test_data.div_(test_data.norm(dim=1, keepdim=True))
+
         for i in range(test_data.shape[0]):
             truth_dist, truth_idx = (data - test_data[i]).norm(dim=1).min(dim=0)
             truth_val = data[truth_idx]
@@ -30,6 +43,7 @@ class MTreeUnitTest(unittest.TestCase):
             dist, val = mtree.get_nearest(test_data[i])
             self.assertAlmostEqual(dist, truth_dist.item())
             self.assertTrue(torch.allclose(val, truth_val))
-            
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     unittest.main()
